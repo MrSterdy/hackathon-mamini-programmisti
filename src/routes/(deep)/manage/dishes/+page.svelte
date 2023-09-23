95629ded65cb40240cc32b8d6fc6945649ea6269
MrSterdy <mrsterdy@gmail.com>
2023-09-23 23:05:47 +0300

    Style manage dishes and users

--- src/routes/(deep)/manage/dishes/+page.svelte
+++ src/routes/(deep)/manage/dishes/+page.svelte
@@ -18,45 +18,48 @@
     } = superForm(data.deleteDishForm);
 </script>
 
-<h1>Меню</h1>
+<section class="flex flex-col gap-3">
+    <h1 class="text-stone-900">Меню</h1>
 
-<ul>
-    {#each data.dishes as dish}
-        <li>
-            <form method="post" action="?/update" use:updateEnhance>
-                <input type="hidden" name="id" value={dish.id} />
+    <ul class="flex flex-col gap-3">
+        {#each data.dishes as dish}
+            <li class="flex flex-col gap-1">
+                <form method="post" action="?/update" use:updateEnhance class="flex flex-col gap-1">
+                    <input type="hidden" name="id" value={dish.id} />
 
-                <input
-                    name="name"
-                    value={dish.name}
-                    type="text"
-                    {...$updateConstraints.name}
-                />
+                    <input
+                        name="name"
+                        class="w-full"
+                        value={dish.name}
+                        type="text"
+                        {...$updateConstraints.name}
+                    />
 
-                <select name="type">
-                    {#each Object.entries(dishTypes) as dishTypeEntry}
-                        <option
-                            value={dishTypeEntry[0]}
-                            selected={dishTypeEntry[0] === dish.type}
+                    <select name="type" class="w-full">
+                        {#each Object.entries(dishTypes) as dishTypeEntry}
+                            <option
+                                value={dishTypeEntry[0]}
+                                selected={dishTypeEntry[0] === dish.type}
                             >{dishTypeEntry[1]}</option>
-                    {/each}
-                </select>
+                        {/each}
+                    </select>
 
-                {#if $updateForm.id === dish.id && $updateMessage}
-                    <span>{$updateMessage}</span>
-                {/if}
+                    {#if $updateForm.id === dish.id && $updateMessage}
+                        <span>{$updateMessage}</span>
+                    {/if}
 
-                <input type="submit" value="Сохранить" />
-            </form>
+                    <input class="btn-black w-full" type="submit" value="Сохранить" />
+                </form>
 
-            <form method="post" action="?/delete" use:deleteEnhance>
-                <input type="hidden" name="id" value={dish.id} />
-                <input type="submit" value="Удалить" />
-            </form>
-        </li>
-    {/each}
-</ul>
+                <form method="post" action="?/delete" use:deleteEnhance>
+                    <input type="hidden" name="id" value={dish.id} />
+                    <input class="btn-black w-full" type="submit" value="Удалить" />
+                </form>
+            </li>
+        {/each}
+    </ul>
 
-<form method="post" action="?/create" use:kitEnhance>
-    <input type="submit" value="Создать блюдо" />
-</form>
+    <form method="post" action="?/create" use:kitEnhance>
+        <input type="submit" value="Создать блюдо" class="btn-black w-full" />
+    </form>
+</section>
--- src/routes/(deep)/manage/users/+page.svelte
+++ src/routes/(deep)/manage/users/+page.svelte
@@ -18,62 +18,68 @@
     );
 </script>
 
-{#if !organizers.length}
-    <h1>Нет организаторов</h1>
-{:else}
-    <h1>Организаторы</h1>
+<section>
+    <div>
+        <h2 class="text-stone-900">Организаторы:</h2>
 
-    <ul>
-        {#each organizers as organizer}
-            <li>
-                <form method="post" use:enhance>
-                    <h2>{organizer.username}</h2>
+        {#if organizers.length}
+            <ul class="flex flex-col gap-3">
+                {#each organizers as organizer}
+                    <li>
+                        <form method="post" class="flex items-center justify-between gap-1" use:enhance>
+                            <h3 class="text-stone-900">{organizer.username}</h3>
 
-                    <input
-                        type="hidden"
-                        name="username"
-                        value={organizer.username}
-                        {...$constraints.username}
-                    />
-                    <input type="hidden" name="action" value="demote" />
+                            <input
+                                type="hidden"
+                                name="username"
+                                value={organizer.username}
+                                {...$constraints.username}
+                            />
+                            <input type="hidden" name="action" value="demote" />
 
-                    {#if $form.username === organizer.username && $message}
-                        <span>{$message}</span>
-                    {/if}
+                            {#if $form.username === organizer.username && $message}
+                                <span class="form-error">{$message}</span>
+                            {/if}
 
-                    <input type="submit" value="Понизить" />
-                </form>
-            </li>
-        {/each}
-    </ul>
-{/if}
+                            <input class="btn-black" type="submit" value="Понизить" />
+                        </form>
+                    </li>
+                {/each}
+            </ul>
+        {:else}
+            <h3 class="text-stone-900">Нет организаторов</h3>
+        {/if}
+    </div>
 
-{#if !users.length}
-    <h1>Нет обычных пользователей</h1>
-{:else}
-    <h1>Обычные пользователи</h1>
+    <div>
+        <h2 class="text-stone-900">Пользователи:</h2>
 
-    <ul>
-        {#each users as user}
-            <li>
-                <form method="post" use:enhance>
-                    <h2>{user.username}</h2>
+        {#if users.length}
+            <ul class="flex flex-col gap-3">
+                {#each users as user}
+                    <li>
+                        <form method="post" use:enhance class="flex items-center justify-between gap-1">
+                            <h3 class="text-stone-900">{user.username}</h3>
 
-                    <input
-                        type="hidden"
-                        name="username"
-                        value={user.username}
-                        {...$constraints.username}
-                    />
-                    <input type="hidden" name="action" value="promote" />
+                            <input
+                                type="hidden"
+                                name="username"
+                                value={user.username}
+                                {...$constraints.username}
+                            />
+                            <input type="hidden" name="action" value="promote" />
 
-                    {#if $form.username === user.username && $message}
-                        <span>{$message}</span>
-                    {/if}
+                            {#if $form.username === user.username && $message}
+                                <span class="form-error">{$message}</span>
+                            {/if}
 
-                    <input type="submit" value="Повысить" />
-                </form>
-            </li>
-        {/each}
-    </ul>
-{/if}
+                            <input class="btn-black" type="submit" value="Повысить" />
+                        </form>
+                    </li>
+                {/each}
+            </ul>
+        {:else}
+            <h3 class="text-stone-900">Нет обычных пользователей</h3>
+        {/if}
+    </div>
+</section>
