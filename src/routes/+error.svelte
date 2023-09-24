3f4856b244eab60a3634e893a5f5fb5e9d33f468
MrSterdy <mrsterdy@gmail.com>
2023-09-24 14:22:29 +0300

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+    <title>Ошибка</title>
+</svelte:head>
+
+<h1 class="text-stone-900 p-3">Ошибка {$page.status}</h1>
